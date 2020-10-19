6aa066b4f6339da2d5a04f29ee4bb7c663c284b8
Andre Smith <andy4swa3@hotmail.com>
2020-10-19 00:27:20 -0700

    ran a console.log to select mu preference for pw characters-sChars-true, emojis-true, nums-flase and lChar-false

--- Develop/script.js
+++ Develop/script.js
@@ -10,23 +10,40 @@
 
   let sChars = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  let emojis = ["😉", "😜", "🤔", "😒", "😃"] 
-  
+  let emojis = ["😉", "😜", "🤔", "😒", "😃"]  
   let lChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   
-  let length = parseInt(prompt("What is the password length?"))
-  
-  console.log(typeof length, length)
+  let potentials = []
 
+  let length = parseInt(prompt("What is the password length?"))
   if (length < 8 || length > 128){
     alert("Invalid input, please pick a number greater than 8 and less 128. Please refresh the page!")
   }
-    else {
-    console.log("continue") 
-    }
+    else {    
+    
     // validated that user selects >8, less 128 characters in length.
-    let chars
+    let lCharsTrue = confirm("Will you chose large characters?")
+    let sCharsTrue = confirm("Will you chose small characters?")
+    let numsTrue = confirm("Will you chose numbers?")
+    let emojisTrue = confirm("Will you chose emojis?")
+    // Did not place confirm for numsTrue and emojis true, so I had to clone from github and start over 
+    
 
+    if (lCharsTrue) { potentials.push(lChars)}
+    if (sCharsTrue) { potentials.push(sChars)}
+    if (numsTrue) { potentials.push(nums)}
+    if (emojisTrue) { potentials.push(emojis)}
+
+    console.log(sCharsTrue, numsTrue, lCharsTrue, emojisTrue)
+
+    // ran a console log for confirm for pw preference and selected emojis and sChars as true; lChars and nums as flase. confirmed per console.log--sending to github
+    }  
+    
+    
+   
+
+
+    
 
 
 
