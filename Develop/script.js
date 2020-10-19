cab37639f5dd02e1876491b21ca6de99f62b7998
Andre Smith <andy4swa3@hotmail.com>
2020-10-18 18:51:12 -0700

    set up pseudo codes w/ outline-steps required to generate the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword() {
+  // ***Below pseudocodes is an outline of the steps to password....
+  
   // How long to you want your password
   // Select which characters you would like to use
   //Select random characters to add to password
   // Return this randomly generated password
   
-var SpecialChar = [ ""]
+  
+let sChars = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let emojis = ["😉", "😜", "🤔", "😒", "😃"]
 
+let lChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+console.log();
 } 
 // Write password to the #password input
 function writePassword() {
